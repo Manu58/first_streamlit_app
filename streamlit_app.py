c3254436d2f9f86839b413b3595b7395e88e58d6
Marc Emanuel <emanuel.marc@gmail.com>
2022-11-20 12:28:49 +0100

    Update streamlit_app.py
    added header and some content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Brealfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Ka;;e, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
