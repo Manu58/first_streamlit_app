782045888625b87cd4d3972f6fdd46da8a6bd59b
Marc Emanuel <emanuel.marc@gmail.com>
2022-11-20 12:29:29 +0100

    Update streamlit_app.py
    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Brealfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Ka;;e, Spinach & Rocket Smoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
