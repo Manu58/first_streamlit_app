e62385a76e56d5e3f2eef830f26a5a4ffc03a273
Marc Emanuel <emanuel.marc@gmail.com>
2022-11-20 21:22:18 +0100

    Update streamlit_app.py
    incorrect

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,5 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanksfor adding ', add_my_fruit)
 
+# nor correct
+my_cur.execute("insert uibto fruit_load_list values('from streamlit')")
